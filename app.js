3852f9c26476a97e47b27a4d1272f58d0cc70715
Cory Friedt <c.friedt89@gmail.com>
2020-05-12 15:05:02 -0700

    added nfl team objects

---
+++ app.js
@@ -0,0 +1,23 @@
+let nflTeams = [
+  { teamName: 'Arizona Cardinals',
+    colors: ['#97233F', '#000000', '#FFB612' ]
+  },
+  { teamName: 'Atlanta Falcons',
+    colors: ['#A71930', '#000000', '#A5ACAF']
+  },
+  { teamName: 'Baltimore Ravens',
+    colors: ['#241773', '#000000', '#9E7C0C']
+  },
+  {
+    teamName: 'Buffalo Bills',
+    colors: ['#00338D', '#C60C30']
+  },
+  {
+    teamName: 'Carolina Panthers',
+    colors: ['#0085CA', '#101820', '#BFC0BF']
+  },
+  {
+    teamName: 'Chicago Bears',
+    colors: ['#0B162A', '#C83803']
+  }
+];
