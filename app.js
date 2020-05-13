0497633d8027f1a3dab7f8ddb5e06fc1da3686e3
Cory Friedt <c.friedt89@gmail.com>
2020-05-12 22:01:47 -0700

    added nfl teams

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // Grab UI Elements
-let colorOne = document.querySelector('.colorOne'),
-    colorTwo = document.querySelector('.colorTwo'),
-    colorThree = document.querySelector('.colorThree'),
-    randomTeamBtn = document.querySelector('.randomTeamButton'),
-    input = document.querySelector('input'),
-    submitBtn = document.querySelector('.submitAnswer');
+const colorOne = document.querySelector('.colorOne'),
+      colorTwo = document.querySelector('.colorTwo'),
+      colorThree = document.querySelector('.colorThree'),
+      randomTeamBtn = document.querySelector('.randomTeamButton'),
+      input = document.querySelector('input'),
+      submitBtn = document.querySelector('.submitAnswer');
+
+// Event Handlers
 
 
 
-let nflTeams = [
+// NFL Teams
+const nflTeams = [
   { teamName: 'Arizona Cardinals',
     colors: ['#97233F', '#000000', '#FFB612' ]
   },
@@ -29,6 +32,57 @@ let nflTeams = [
   {
     teamName: 'Chicago Bears',
     colors: ['#0B162A', '#C83803']
+  },
+  {
+    teamName: 'Cincinnati Bengals',
+    colors: ['#FB4F14', '#000000']
+  },
+  {
+    teamName: 'Cleveland Browns',
+    colors: ['#311D00', '#FF3C00']
+  },
+  {
+    teamName: 'Dallas Cowboys',
+    colors: ['#003594', '#041E42', '#869397']
+  },
+  {
+    teamName: 'Denver Broncos',
+    colors: ['#FB4F14', '#002244']
+  },
+  {
+    teamName: 'Detroit Lions',
+    colors: ['#0076B6', '#B0B7BC', '#000000']
+  },
+  {
+    teamName: 'Green Bay Packers',
+    colors: ['#203731', '#FFB612']
+  },
+  {
+    teamName: 'Houston Texans',
+    colors: ['#03202F', '#A71930']
+  },
+  {
+    teamName: 'Indianapolis Colts',
+    colors: ['#002C5F','#A2AAAD']
+  },
+  {
+    teamName: 'Jacksonville Jaguars',
+    colors: ['#101820', '#D7A22A', '#006778']
+  },
+  {
+    teamName: 'Kansas City Chiefs',
+    colors: ['#E31837', '#FFB81C']
+  },
+  {
+    teamName: 'Los Angeles Chargers',
+    colors: ['#002A5E', '#FFC20E', '#0080C6']
+  },
+  {
+    teamName: 'Los Angeles Rams',
+    colors: ['#002244', '#866D4B']
+  },
+  {
+
   }
 ];
 
